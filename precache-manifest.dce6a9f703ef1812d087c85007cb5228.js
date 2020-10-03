bad2a4d6f66ce5b4c8f454a66eda4f381c6414dc
d-putnam <dan@dputnam.net>
2020-10-03 16:17:05 -0400

    Updates

---
+++ precache-manifest.dce6a9f703ef1812d087c85007cb5228.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347c4078336be54fed16535d0f91882e",
+    "url": "/uwuVS/index.html"
+  },
+  {
+    "revision": "c3001a414b9e1bc5237e",
+    "url": "/uwuVS/static/css/main.cadd5a6c.chunk.css"
+  },
+  {
+    "revision": "fa1fe14e10052314b892",
+    "url": "/uwuVS/static/js/2.7ba2fd14.chunk.js"
+  },
+  {
+    "revision": "0aadc8a8405aeed76ae7e020b1d921e7",
+    "url": "/uwuVS/static/js/2.7ba2fd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3001a414b9e1bc5237e",
+    "url": "/uwuVS/static/js/main.c88bbe1f.chunk.js"
+  },
+  {
+    "revision": "f3c62319fcf1d0dd916c",
+    "url": "/uwuVS/static/js/runtime-main.6bc6d4ef.js"
+  },
+  {
+    "revision": "56eb48ac21f4e5444629413e2060d90a",
+    "url": "/uwuVS/static/media/fx.56eb48ac.svg"
+  },
+  {
+    "revision": "a6a8ebcd1b270de9193d90d65e4bac59",
+    "url": "/uwuVS/static/media/info.a6a8ebcd.svg"
+  },
+  {
+    "revision": "7f8f9cd36fcc5a6f4b34aecca57a7e03",
+    "url": "/uwuVS/static/media/presets.7f8f9cd3.svg"
+  },
+  {
+    "revision": "952455057cfec925540c8fc9a46fb00a",
+    "url": "/uwuVS/static/media/shaders.95245505.svg"
+  },
+  {
+    "revision": "8dc8abecdd5b36d00ecaf91d862d28ba",
+    "url": "/uwuVS/static/media/sine.8dc8abec.svg"
+  }
+]);
